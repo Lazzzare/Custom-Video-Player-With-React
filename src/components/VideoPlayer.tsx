2717f982c3b013ee145a8a142a7b50188290d025
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-02-25 04:19:39 +0400

    install framer-motion and add animations (kai rame gamova mgoni)

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -10,11 +10,16 @@ import {
 
 import videoFile from "../../video.mp4";
 import { DarkMode } from "../type";
+import { motion } from "framer-motion";
 
 const VideoPlayer = ({ darkMode, setDarkMode }: DarkMode) => {
   const [playButton, setPlayButton] = useState<boolean>(true);
   const [fullScreen] = useState<boolean>(false);
   const [settingsMenu, setSettingsMenu] = useState<boolean>(false);
+  const [settingsHoverText, setSettingsHoverText] = useState<string>("");
+  const [playHoverText, setPlayHoverText] = useState<string>("");
+  const [pauseHoverText, setPauseHoverText] = useState<string>("");
+  const [fullScreenHoverText, setFullScreenHoverText] = useState<string>("");
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   //   handlePlayPause
@@ -48,65 +53,132 @@ const VideoPlayer = ({ darkMode, setDarkMode }: DarkMode) => {
   return (
     <div className="relative">
       {/* Video */}
-      <video
-        className="rounded-xl"
-        onClick={() => {
-          handlePlayPause();
-          setPlayButton(!playButton);
-        }}
-        ref={videoRef as React.RefObject<HTMLVideoElement>}
-        src={videoFile}
-        controls={false}
-        preload="metadata"
-      ></video>
+      <motion.div
+        initial={{ opacity: 0, y: -500 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <video
+          className="rounded-xl"
+          onClick={() => {
+            handlePlayPause();
+            setPlayButton(!playButton);
+          }}
+          ref={videoRef as React.RefObject<HTMLVideoElement>}
+          src={videoFile}
+          controls={false}
+          preload="metadata"
+        ></video>
+      </motion.div>
       {/* Play Button */}
-      {playButton ? (
-        <button
-          onClick={() => {
-            handlePlayPause();
-            setPlayButton(!playButton);
-          }}
-          className="text-white absolute bottom-3 left-3"
-        >
-          <IoMdPlay />
-        </button>
-      ) : (
-        // Pause Button
-        <button
-          onClick={() => {
-            handlePlayPause();
-            setPlayButton(!playButton);
-          }}
-          className="text-white absolute bottom-3 left-3"
-        >
-          <FaPause />
-        </button>
-      )}
+      <motion.div
+        initial={{ opacity: 0, x: -200 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        {playButton ? (
+          <button
+            onClick={() => {
+              handlePlayPause();
+              setPlayButton(!playButton);
+            }}
+            className="text-white absolute bottom-3 left-3"
+          >
+            <div className="flex flex-col items-start gap-3">
+              <span>{playHoverText && playHoverText}</span>
+              <IoMdPlay
+                onMouseEnter={() =>
+                  playHoverText ? null : setPlayHoverText("Play")
+                }
+                onMouseLeave={() => setPlayHoverText("")}
+              />
+            </div>
+          </button>
+        ) : (
+          // Pause Button
+          <button
+            onClick={() => {
+              handlePlayPause();
+              setPlayButton(!playButton);
+            }}
+            className="text-white absolute bottom-3 left-3"
+          >
+            <div className="flex flex-col items-start gap-3">
+              <span>{pauseHoverText && pauseHoverText}</span>
+              <FaPause
+                onMouseEnter={() =>
+                  pauseHoverText ? null : setPauseHoverText("Pause")
+                }
+                onMouseLeave={() => setPauseHoverText("")}
+              />
+            </div>
+          </button>
+        )}
+      </motion.div>
       {/* FullScreenButton */}
-      <BsFullscreen
-        onClick={handleFullScreen}
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
         className="absolute bottom-3 right-3 text-white cursor-pointer"
-      />
+      >
+        <div className="flex flex-col items-end gap-4">
+          <span className="text-sm">{fullScreenHoverText}</span>
+          <BsFullscreen
+            onClick={handleFullScreen}
+            onMouseEnter={() =>
+              settingsMenu ? null : setFullScreenHoverText("FullScreen")
+            }
+            onMouseLeave={() => setFullScreenHoverText("")}
+          />
+        </div>
+      </motion.div>
       {/* Settings */}
       <div className="relative">
-        <CiSettings
-          onClick={() => setSettingsMenu(!settingsMenu)}
-          className="absolute bottom-[9px] right-12 text-[22px] text-white cursor-pointer"
-        />
-        {settingsMenu ? (
-          <div
-            onClick={() => setDarkMode(!darkMode)}
-            className="absolute bg-white p-4 bottom-10 right-14 rounded-xl flex items-center gap-2 cursor-pointer"
+        <motion.div className="absolute bottom-[9px] right-12 text-[22px] text-white cursor-pointer">
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
           >
-            <h1>DarkMode</h1>
-            {darkMode ? (
-              <RiCheckboxCircleLine />
-            ) : (
-              <RiCheckboxBlankCircleLine
-              // onClick={() => setDarkMode(!darkMode)}
+            <div className="flex flex-col items-end gap-4">
+              <span className="text-sm">{settingsHoverText}</span>
+              <CiSettings
+                className="hover:animate-spin"
+                onMouseEnter={() =>
+                  settingsMenu ? null : setSettingsHoverText("Settings")
+                }
+                onMouseLeave={() => setSettingsHoverText("")}
+                onClick={() => {
+                  setSettingsMenu(!settingsMenu);
+                }}
               />
-            )}
-          </div>
+            </div>
+          </motion.div>
+        </motion.div>
+        {settingsMenu ? (
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <div
+              onClick={() => setDarkMode(!darkMode)}
+              className={`absolute ${
+                darkMode
+                  ? "bg-slate-900 border border-bg-slate-300 outline-none text-white"
+                  : "bg-slate-300 text-bg-slate-900 border border-gray-800"
+              } p-4 bottom-10 right-14 rounded-xl flex items-center gap-2 cursor-pointer`}
+            >
+              <h1>DarkMode</h1>
+
+              {darkMode ? (
+                <RiCheckboxCircleLine />
+              ) : (
+                <RiCheckboxBlankCircleLine />
+              )}
+            </div>
+          </motion.div>
         ) : null}
       </div>
     </div>
